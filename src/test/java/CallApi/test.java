acf5f89b8c63f4cfb5eb96d3c471084a3722c593
nguyentrunghieu71199 <trunghieugtvt71199@gmail.com>
2022-01-08 22:11:47 +0700

    no message

--- src/test/java/CallApi/test.java
+++ src/test/java/CallApi/test.java
@@ -7,6 +7,6 @@ public class test {
         String a2= "hieu";
         System.out.println(stringBuffer.append(a2));
 
-        System.out.println("Hieu 1234456");
+        System.out.println("Hieu 1234456dsd");
     }
 }
